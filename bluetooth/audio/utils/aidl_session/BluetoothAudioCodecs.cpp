41c0733ec6c16cf868e388347b1ee92cd1e9abc9
jimmy.chen <jimmy.chen@savitech.co>
2022-12-23 03:50:06 -0500

    interfaces: Add LHDCV235 codec
    1. LHDC V5.0.9 non-offload - : non-QA version
    2. no codec shared libs (contact with Savitech)
    
    Change-Id: I81f8633a367be89fda53435d222907b83ba1ebd2
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- bluetooth/audio/utils/aidl_session/BluetoothAudioCodecs.cpp
+++ bluetooth/audio/utils/aidl_session/BluetoothAudioCodecs.cpp
@@ -41,7 +41,7 @@ namespace bluetooth {
 namespace audio {
 
 static const PcmCapabilities kDefaultSoftwarePcmCapabilities = {
-    .sampleRateHz = {16000, 24000, 32000, 44100, 48000, 88200, 96000},
+    .sampleRateHz = {16000, 24000, 32000, 44100, 48000, 88200, 96000, 192000}, //Savitech LHDC 192KHz
     .channelMode = {ChannelMode::MONO, ChannelMode::STEREO},
     .bitsPerSample = {16, 24, 32},
     .dataIntervalUs = {},
